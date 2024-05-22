d3126acf4858ec5e82499c0328f37f24ddb28252
Veendel <144378228+NagyVendel@users.noreply.github.com>
2024-05-22 23:20:55 +0200

    sok minden

---
+++ NagyVeendel/nagyv-main/assets/kocsioverlay/overlay.js
@@ -0,0 +1,7 @@
+function kocsiopen() {
+    document.getElementById("hoveroverlay").style.width = "100%";
+  }
+  
+  function kocsiclose() {
+    document.getElementById("hoveroverlay").style.width = "0%";
+  }
