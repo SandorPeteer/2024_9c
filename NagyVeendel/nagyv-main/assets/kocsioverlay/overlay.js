6a2c3caff37a17e4f1d5c98c0cfa137db4953111
Veendel <144378228+NagyVendel@users.noreply.github.com>
2024-05-23 22:16:56 +0200

    Sok cucc

--- NagyVeendel/nagyv-main/assets/kocsioverlay/overlay.js
+++ NagyVeendel/nagyv-main/assets/kocsioverlay/overlay.js
@@ -1,7 +1,56 @@
-function kocsiopen() {
-    document.getElementById("hoveroverlay").style.width = "60vw";
+function kocsiopenmustang() {
+    document.getElementById("hoveroverlaymustang").style.width = "60vw";
+    document.getElementById("fulloverlay").style.width = "100%";
+    document.getElementById("kocsik01").style.opacity = "1";
   }
   
-  function kocsiclose() {
-    document.getElementById("hoveroverlay").style.width = "0%";
+  function kocsiclosemustang() {
+    document.getElementById("hoveroverlaymustang").style.width = "0%";
+    document.getElementById("fulloverlay").style.width = "0%";
+    document.getElementById("kocsik01").style.opacity = "0";
   }
+
+
+  function kocsiopenpuma() {
+    document.getElementById("hoveroverlaypuma").style.width = "60vw";
+    document.getElementById("fulloverlay").style.width = "100%";
+    document.getElementById("kocsik02").style.opacity = "1";
+  }
+  
+  function kocsiclosepuma() {
+    document.getElementById("hoveroverlaypuma").style.width = "0%";
+    document.getElementById("fulloverlay").style.width = "0%";
+    document.getElementById("kocsik02").style.opacity = "0";
+  }
+
+
+  function kocsiopenfiesta() {
+    document.getElementById("hoveroverlayfiesta").style.width = "60vw";
+    document.getElementById("fulloverlay").style.width = "100%";
+    document.getElementById("kocsik03").style.opacity = "1";
+  }
+  
+  function kocsiclosefiesta() {
+    document.getElementById("hoveroverlayfiesta").style.width = "0%";
+    document.getElementById("fulloverlay").style.width = "0%";
+    document.getElementById("kocsik03").style.opacity = "0";
+  }
+
+  function kocsiopenfocus() {
+    document.getElementById("hoveroverlayfocus").style.width = "60vw";
+    document.getElementById("fulloverlay").style.width = "100%";
+    document.getElementById("kocsik04").style.opacity = "1";
+    }
+  
+  function kocsiclosefocus() {
+    document.getElementById("hoveroverlayfocus").style.width = "0%";
+    document.getElementById("fulloverlay").style.width = "0%";
+    document.getElementById("kocsik04").style.opacity = "0";
+  }
+
+function startanimation(element) {
+  element.classList.add("fulloverlay1");
+  element.addEventListener( "animationend",  function() {
+    element.classList.remove("fulloverlay1");  	
+  } );
+}
