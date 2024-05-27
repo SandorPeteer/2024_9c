1b0d312ee7010eaf1845dfeb77ae9920ff7caa9f
Veendel <144378228+NagyVendel@users.noreply.github.com>
2024-05-27 20:39:14 +0200

    qweqwqwewe

--- NagyVeendel/nagyv-main/assets/kocsioverlay/overlay.js
+++ NagyVeendel/nagyv-main/assets/kocsioverlay/overlay.js
@@ -1,43 +1,44 @@
 function kocsiopenmustang() {
-    document.getElementById("hoveroverlaymustang").style.width = "60vw";
+    document.getElementById("hoveroverlaymustang").style.width = "70vw";
     document.getElementById("fulloverlay").style.width = "100%";
     document.getElementById("kocsik01").style.opacity = "1";
   }
   
   function kocsiclosemustang() {
-    document.getElementById("hoveroverlaymustang").style.width = "0%";
+    document.getElementById("hoveroverlaymustang").style.width = "0vw";
     document.getElementById("fulloverlay").style.width = "0%";
     document.getElementById("kocsik01").style.opacity = "0";
   }
 
 
   function kocsiopenpuma() {
-    document.getElementById("hoveroverlaypuma").style.width = "60vw";
+    document.getElementById("hoveroverlaypuma").style.width = "70vw";
     document.getElementById("fulloverlay").style.width = "100%";
     document.getElementById("kocsik02").style.opacity = "1";
   }
   
   function kocsiclosepuma() {
-    document.getElementById("hoveroverlaypuma").style.width = "0%";
+    document.getElementById("hoveroverlaypuma").style.width = "0vw";
     document.getElementById("fulloverlay").style.width = "0%";
     document.getElementById("kocsik02").style.opacity = "0";
   }
 
 
   function kocsiopenfiesta() {
-    document.getElementById("hoveroverlayfiesta").style.width = "60vw";
+    document.getElementById("hoveroverlayfiesta").style.width = "70vw";
     document.getElementById("fulloverlay").style.width = "100%";
     document.getElementById("kocsik03").style.opacity = "1";
   }
   
   function kocsiclosefiesta() {
-    document.getElementById("hoveroverlayfiesta").style.width = "0%";
+    document.getElementById("hoveroverlayfiesta").style.width = "0vw";
     document.getElementById("fulloverlay").style.width = "0%";
     document.getElementById("kocsik03").style.opacity = "0";
+
   }
 
   function kocsiopenfocus() {
-    document.getElementById("hoveroverlayfocus").style.width = "60vw";
+    document.getElementById("hoveroverlayfocus").style.width = "70vw";
     document.getElementById("fulloverlay").style.width = "100%";
     document.getElementById("kocsik04").style.opacity = "1";
     }
@@ -46,11 +47,5 @@ function kocsiopenmustang() {
     document.getElementById("hoveroverlayfocus").style.width = "0%";
     document.getElementById("fulloverlay").style.width = "0%";
     document.getElementById("kocsik04").style.opacity = "0";
-  }
 
-function startanimation(element) {
-  element.classList.add("fulloverlay1");
-  element.addEventListener( "animationend",  function() {
-    element.classList.remove("fulloverlay1");  	
-  } );
-}
+  }
