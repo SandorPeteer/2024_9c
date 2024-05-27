efc3e21355b2e5ba7aef47c8e5e3f33ed4fafdf8
xmateka007 <165039533+xmateka007@users.noreply.github.com>
2024-05-27 19:28:02 +0200

    Add files via upload

---
+++ Voronamáté/maserati.js
@@ -0,0 +1,7 @@
+document.getElementById('newsletterForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+var név = document.getElementById('név').value;
+var email = document.getElementById('email').value;
+var adatvédelem = document.getElementById('adatvédelem').checked;
+var üzenet = document.getElementById('formÜzenet');
+})
